71c7c6a78cf1ebb68cb4d13a29f530ff622bd536
yehs1225 <yeh.mentos@gmail.com>
2022-02-22 22:06:58 +0800

    add JS prototype chain and this.

---
+++ assets/js/a7023ddc.295c01b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1713],{3457:function(s){s.exports=JSON.parse('{"/blog/tags/mindset":{"allTagsPath":"/blog/tags","slug":"/blog/tags/mindset","name":"mindset","count":3,"permalink":"/blog/tags/mindset"}}')}}]);
